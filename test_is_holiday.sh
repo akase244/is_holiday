da6f7a2f1567c31c48ce4c0b7cd76e0c7b0da8ad
akase244 <akase244@gmail.com>
2021-07-28 23:30:42 +0900

    変数名を小文字に統一

--- test_is_holiday.sh
+++ test_is_holiday.sh
@@ -12,8 +12,8 @@ failures=0
 # 引数なし
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday`
-if [[ !$RESULT ]] ; then
+result=`is_holiday`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -24,8 +24,8 @@ fi
 # 引数が空
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday ""`
-if [[ !$RESULT ]] ; then
+result=`is_holiday ""`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -36,8 +36,8 @@ fi
 # 引数が日付の形式ではない
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 1234567`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 1234567`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -48,8 +48,8 @@ fi
 # 引数が日付の形式ではない
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 12345678`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 12345678`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -60,8 +60,8 @@ fi
 # 祝日に該当する（ハイフン区切り）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021-1-1`
-if [[ $RESULT ]] ; then
+result=`is_holiday 2021-1-1`
+if [[ $result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -72,8 +72,8 @@ fi
 # 祝日に該当する（スラッシュ区切り）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/1/1`
-if [[ $RESULT ]] ; then
+result=`is_holiday 2021/1/1`
+if [[ $result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -84,8 +84,8 @@ fi
 # 祝日に該当する（月が前ゼロ）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/01/1`
-if [[ $RESULT ]] ; then
+result=`is_holiday 2021/01/1`
+if [[ $result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -96,8 +96,8 @@ fi
 # 祝日に該当する（日が前ゼロ）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/1/01`
-if [[ $RESULT ]] ; then
+result=`is_holiday 2021/1/01`
+if [[ $result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -108,8 +108,8 @@ fi
 # 祝日に該当する（月が前ゼロ、日が前ゼロ）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/01/01`
-if [[ $RESULT ]] ; then
+result=`is_holiday 2021/01/01`
+if [[ $result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -120,8 +120,8 @@ fi
 # 祝日に該当しない（うるう年、祝日ではない）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2020/2/29`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 2020/2/29`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -132,8 +132,8 @@ fi
 # 引数が不正な日付（存在しない日付）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/2/29`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 2021/2/29`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -144,8 +144,8 @@ fi
 # 祝日に該当しない（存在する日付、祝日ではない）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/6/30`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 2021/6/30`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -156,8 +156,8 @@ fi
 # 引数が不正な日付（存在しない日付）
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/6/31`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 2021/6/31`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -168,8 +168,8 @@ fi
 # 祝日に該当する
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/7/22`
-if [[ $RESULT ]] ; then
+result=`is_holiday 2021/7/22`
+if [[ $result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -180,8 +180,8 @@ fi
 # 引数が不正な日付
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/99/99`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 2021/99/99`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -192,8 +192,8 @@ fi
 # 引数が日付の形式ではない
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 2021/725`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 2021/725`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
@@ -204,8 +204,8 @@ fi
 # 引数が日付の形式ではない
 tests=`expr $tests + 1`
 echo "Tests:${tests}"
-RESULT=`is_holiday 20217/25`
-if [[ !$RESULT ]] ; then
+result=`is_holiday 20217/25`
+if [[ !$result ]] ; then
     echo 'SUCCESS'
     success=`expr $success + 1`
 else
